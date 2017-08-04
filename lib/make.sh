ee38aa2af9d8dea99448090ef41ac82e81f43f72
zxxdls <jiangyiran1997@outlook.com>
2017-08-05 02:15:26 +0800

    123

--- lib/make.sh
+++ lib/make.sh
@@ -10,13 +10,13 @@ nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
 	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 
 ## if you install tf using already-built binary, or gcc version 4.x, uncomment the two lines below
+#g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o roi_pooling.so roi_pooling_op.cc \
+#	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+
+ for gcc5-built tf
 g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o roi_pooling.so roi_pooling_op.cc \
 	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
-
-# for gcc5-built tf
-#g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=1 -o roi_pooling.so roi_pooling_op.cc \
-	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
-#cd ..
+cd ..
 
 
 # add building psroi_pooling layer
@@ -28,7 +28,7 @@ g++ -std=c++11 -shared -o psroi_pooling.so psroi_pooling_op.cc \
 	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
 
 ## if you install tf using already-built binary, or gcc version 4.x, uncomment the two lines below
-g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o psroi_pooling.so psroi_pooling_op.cc \
-	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+#g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o psroi_pooling.so psroi_pooling_op.cc \
+#	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
 
 cd ..
